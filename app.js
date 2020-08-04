94845d0c16b7b9806d75f61338b0592d24cc98ba
Zach Smith <rysskismith@gmail.com>
2020-08-04 13:22:00 -0500

    rough draft complete

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
+const { projects } = require("./data.json");
 
-const  { projects } = require('./data.json');
-
-
-app.set('view engine', 'pug')
-
-app.use( '/static', express.static('public') );
-
-app.get('/', (req, res) => {
-
-        res.render('index', { projects });
+app.set("view engine", "pug");
+app.use("/static", express.static("public"));
+app.get("/", (req, res) => {
+  res.render("index", { projects });
 });
 
-app.get('/about', (req, res) => {
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+// Checks if project IDs exist
+app.get("/projects/:id", (req, res) => {
+  const projectId = req.params.id;
+  const project = projects.find(({ id }) => id === +projectId);
 
-        res.render('about');
+  if (project) {
+    res.render("project", { project });
+  } else {
+    res.sendStatus(404);
+  }
 });
 
-    app.get('/projects/:id', (req, res) => {
-        const projectId = req.params.id;
-        const project = projects.find( ({ id }) => id === +projectId );
-        
-        if (project) {
-          res.render('project', { project });
-        } else {
-          res.sendStatus(404);
-        }
-      });
+// Error Handler
+app.use((req, res, next) => {
+  const err = new Error("Oh no! This page does not exist");
+  console.log("Unfortunately this page does not exist");
+  err.status = 404;
+  next(err);
+});
 
-      app.listen(3000, () => {
-        console.log('Hear you loud and clear on localhost:3000!')
-    });
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status || 500);
+  res.render("error", err);
+});
 
+app.listen(3000, () => {
+  console.log("Hear you loud and clear on localhost:3000!");
+});
