ddc9d4aef21c5d1df277068f72db6b55e723d6b5
Zach Smith <rysskismith@gmail.com>
2020-08-04 21:57:38 -0500

    ready for grading

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Static Node.js and Express Site
- This is a simple portfolio
+
+## Installation 
+
+In your command line interface of choice, run "npm install" to install all of this project's dependencies. Then, simply run "npm start" to start the server.
+
+### How to Use
+
+This is a portfolio site that showcases all of my previous JavaScript projects. By clicking on a project, a page is dynamically generated that shows additional photos of the project as well as links to a live version of each project as well as its GitHub repository. On the landing page, users can click "learn more" in the left sidebar to show additional information about myself such as previous accomplishments, skills, and contact information. This project forms a solid foundation for any junior developer that wants to consolidate all of their accomplishments in one location. 
+
+
+## Built With
+
+* Vanilla JavaScript
+* Express
--- app.js
+++ app.js
@@ -14,23 +14,26 @@ app.get("/about", (req, res) => {
 // Checks if project IDs exist
 app.get("/projects/:id", (req, res) => {
   const projectId = req.params.id;
-  const project = projects.find(({ id }) => id === +projectId);
-    res.render("project", { project });
-  
+  const project = projects[projectId];
+  res.render("project", { project });
 });
 
-// Error Handler
+// 404 Error Handler
 app.use((req, res, next) => {
-  const err = new Error("Oh no! This page does not exist");
   console.log("Unfortunately this page does not exist");
+  const err = new Error();
+  err.message = "Oh no! This page does not exist";
   err.status = 404;
   next(err);
 });
 
+// Global Error Handler
 app.use((err, req, res, next) => {
-  res.locals.error = err;
-  res.status(err.status || 500);
-  res.render("error", err);
+  console.log(`Global err handler caught a ${err.name}`);
+  err.message = `Oops!  It looks like there's been a ${err.name} on the server.`;
+  err.status = err.status || 500;
+  res.status(err.status);
+  res.render("error", { err });
 });
 
 app.listen(3000, () => {
