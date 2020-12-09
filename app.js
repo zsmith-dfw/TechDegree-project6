1ee01cffda2a9311c6504d9d0e5971b6b94ff7a4
Zach Smith <65558154+zsmith-dfw@users.noreply.github.com>
2020-12-09 15:28:17 -0600

    changed to a dynamic port variable

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ app.use((err, req, res, next) => {
   res.render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("Hear you loud and clear on localhost:3000!");
-});
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Hear you loud and clear on: ${port}`));
