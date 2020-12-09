585f3c599c47465f1343a0bb684c2c04a09a35c3
Zach Smith <65558154+zsmith-dfw@users.noreply.github.com>
2020-12-09 15:35:13 -0600

    corrected port

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ app.use((err, req, res, next) => {
   res.render("error", { err });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Hear you loud and clear on: ${port}`));
